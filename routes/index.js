192e9798767a53a57fb10c97c53d087a5834faae
Chen Yang <ericfriday@outlook.com>
2016-02-18 20:11:14 +0000

    add experiment header

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,19 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
-exports.view = function(req, res){
-	projects["grid"] = false;
-  	res.render('index', projects);
-};
+ exports.view = function(req, res){
 
-exports.viewGrid = function(req, res){
-	projects["grid"] = true;
-  	res.render('index', projects);
-};
+ 	var random_num = Math.random();
+ 	console.log(random_num);
+ 	if (random_num > 0.5) {
+ 		projects['grid'] = false;
+ 		res.render('index', projects);
+ 	} else {
+ 		res.redirect('/grid');
+ 	}
+ };
+
+ exports.viewGrid = function(req, res){
+ 	projects["grid"] = true;
+ 	res.render('index', projects);
+ };
